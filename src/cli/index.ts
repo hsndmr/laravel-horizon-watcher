a0fa35e73f1d366d5a18062a6e187b662c420831
Hasan Demir <hsndmr5044@hotmail.com>
2022-12-12 21:21:25 +0300

    Add commands to start laravel-horizon-watcher

---
+++ src/cli/index.ts
@@ -0,0 +1,33 @@
+import { Cli } from './cli';
+
+const cli = new Cli();
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const yargs = require('yargs')
+  .usage('Usage: laravel-horizon-auto-restart <command> [options]')
+  .command(
+    'start [php] [path]',
+    'start the server',
+    (yargs) => {
+      return yargs
+        .positional('php', {
+          describe: 'php interpreter path',
+          default: 'php',
+        })
+        .positional('paths', {
+          describe: 'paths to watch',
+          default: '',
+        });
+    },
+    (argv) => {
+      if (argv.verbose) console.info(`start server on :${argv.port}`);
+      cli.start(argv);
+    }
+  )
+  .demandCommand(1, 'Please provide a valid command.')
+  .help('help')
+  .alias('help', 'h');
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const argv = yargs.argv;
